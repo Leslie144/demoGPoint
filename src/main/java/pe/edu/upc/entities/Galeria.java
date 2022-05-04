2f17fede790faa5933075c1c4810c66fe13d87e0
leslie2952 <85373184+Leslie144@users.noreply.github.com>
2022-05-04 13:12:22 -0500

    cmb

--- src/main/java/pe/edu/upc/entities/Galeria.java
+++ src/main/java/pe/edu/upc/entities/Galeria.java
@@ -1,5 +1,16 @@
 package pe.edu.upc.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Galeria")
 public class Galeria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
 
 }
